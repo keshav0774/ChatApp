a5c828a723217f0833b422eb1208e61c8f161260
Keshav Mishra <keshav0774@gmail.com>
2026-07-31 20:12:28 +0530

    message schema created

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ import express from 'express';
 import dotenv from "dotenv"
 import connectToMongoDb from './config/database.js';
 import dns from 'dns';
+import cookieParser from 'cookie-parser';
 dns.setServers(['8.8.8.8','8.8.4.4'])
 
 
 dotenv.config();
 const app = express();
 app.use(express.json());
-
+app.use(cookieParser());
 
 
 const Port = 3000;
